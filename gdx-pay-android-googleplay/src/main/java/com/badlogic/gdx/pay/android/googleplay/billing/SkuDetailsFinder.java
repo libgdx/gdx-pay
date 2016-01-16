a1d30d879316e6f5f51177407f89ee3e9ce9ea54
Kees van Dieren <keesvandieren@squins.com>
2016-01-16 08:36:21 +0100

    #80 added todo

--- gdx-pay-android-googleplay/src/main/java/com/badlogic/gdx/pay/android/googleplay/billing/SkuDetailsFinder.java
+++ gdx-pay-android-googleplay/src/main/java/com/badlogic/gdx/pay/android/googleplay/billing/SkuDetailsFinder.java
@@ -1,5 +1,6 @@
 package com.badlogic.gdx.pay.android.googleplay.billing;
 
+// TODO: only needed because Information does not provide enough details.
 public interface SkuDetailsFinder {
 
     SkuDetails getSkuDetails(String productId);
