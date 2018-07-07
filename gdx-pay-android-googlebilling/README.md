e8abce3f00af12f6adb9fb83d700a9224fb7dfae
Benjamin Schulte <bs@golfgl.de>
2018-07-07 21:01:16 +0200

    Google Billing: Log in, fetch offers

---
+++ gdx-pay-android-googlebilling/README.md
@@ -0,0 +1,17 @@
+# InApp purchasing implementation for Google Play
+
+Handles purchases and restores for non-consumable and consumable products, will not work as intended for subscriptions.
+
+## Why yet another implementation?
+
+* OpenIAB is currently unmaintained and has open issues, making payments fail too often
+* gdx-pay-android-googleplay uses Google's old AIDL-based approach while this implementation uses the convinient Google Play Billing Library which handles all the complicated stuff.
+* No need to change ProGuard's config
+* No need to change your AndroidManifest
+* No unneeded dependencies included - your app will grow just about 30KB with this lib!
+
+Hint: Probably you have to add
+
+         maven { url "https://jcenter.bintray.com" }
+
+to your Gradle file's repository paragraph.
--- gdx-pay-android-googlebilling/src/com/badlogic/gdx/pay/android/googlebilling/PurchaseManagerGoogleBilling.java
+++ gdx-pay-android-googlebilling/src/com/badlogic/gdx/pay/android/googlebilling/PurchaseManagerGoogleBilling.java
@@ -1,35 +1,158 @@
 package com.badlogic.gdx.pay.android.googlebilling;
 
+import android.app.Activity;
+import android.support.annotation.Nullable;
+
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.pay.Information;
 import com.badlogic.gdx.pay.PurchaseManager;
 import com.badlogic.gdx.pay.PurchaseManagerConfig;
 import com.badlogic.gdx.pay.PurchaseObserver;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
- * The purchase manager implementation for Google Play (Android) using Google Billing Library.
+ * The purchase manager implementation for Google Play (Android) using Google Billing Library
+ * <p>
+ * https://developer.android.com/google/play/billing/billing_java_kotlin
  * <p>
  * Created by Benjamin Schulte on 07.07.2018.
  */
 
-public class PurchaseManagerGoogleBilling implements PurchaseManager {
+public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesUpdatedListener {
+    private static final String TAG = "GdxPay/GoogleBilling";
+    private final BillingClient mBillingClient;
+    private final Map<String, Information> informationMap = new ConcurrentHashMap<String, Information>();
+    private boolean serviceConnected;
+    private boolean installationComplete;
+    private PurchaseObserver observer;
+    private PurchaseManagerConfig config;
+
+    public PurchaseManagerGoogleBilling(Activity activity) {
+        mBillingClient = BillingClient.newBuilder(activity).setListener(this).build();
+    }
+
     @Override
     public Information getInformation(String identifier) {
-        return null;
+        Information information = informationMap.get(identifier);
+        return information == null ? Information.UNAVAILABLE : information;
     }
 
     @Override
     public String storeName() {
-        return null;
+        return PurchaseManagerConfig.STORE_NAME_ANDROID_GOOGLE;
     }
 
     @Override
-    public void install(PurchaseObserver observer, PurchaseManagerConfig config, boolean autoFetchInformation) {
+    public void install(final PurchaseObserver observer, PurchaseManagerConfig config, final boolean
+            autoFetchInformation) {
+        this.observer = observer;
+        this.config = config;
 
+        // make sure to call the observer again
+        installationComplete = false;
+
+        startServiceConnection(new Runnable() {
+            @Override
+            public void run() {
+                if (!serviceConnected)
+                    observer.handleInstallError(new RuntimeException("Connection to Play Billing not possible"));
+                else if (autoFetchInformation) {
+                    fetchOfferDetails();
+                } else
+                    setInstalledAndNotifyObserver();
+            }
+        });
+    }
+
+    public void startServiceConnection(final Runnable excecuteOnSetupFinished) {
+        mBillingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(int billingResponseCode) {
+                Gdx.app.debug(TAG, "Setup finished. Response code: " + billingResponseCode);
+
+                serviceConnected = (billingResponseCode == BillingClient.BillingResponse.OK);
+
+                if (excecuteOnSetupFinished != null) {
+                    excecuteOnSetupFinished.run();
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                serviceConnected = false;
+            }
+        });
+    }
+
+    private void fetchOfferDetails() {
+        int offerSize = config.getOfferCount();
+        List skuList = new ArrayList<>(offerSize);
+        for (int z = 0; z < config.getOfferCount(); z++) {
+            skuList.add(config.getOffer(z).getIdentifierForStore(storeName()));
+        }
+
+        if (skuList.size() > 0) {
+
+            SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+            params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+            mBillingClient.querySkuDetailsAsync(params.build(),
+                    new SkuDetailsResponseListener() {
+                        @Override
+                        public void onSkuDetailsResponse(int responseCode, List<SkuDetails> skuDetailsList) {
+                            if (responseCode != BillingClient.BillingResponse.OK) {
+                                Gdx.app.error(TAG, "onSkuDetailsResponse failed, error code is " + responseCode);
+                                if (!installationComplete)
+                                    observer.handleInstallError(new RuntimeException("onSkuDetailsResponse failed, " +
+                                            "status code is " + responseCode));
+
+                            } else {
+                                if (skuDetailsList != null) {
+                                    for (SkuDetails skuDetails : skuDetailsList) {
+                                        informationMap.put(skuDetails.getSku(), convertSkuDetailsToInformation
+                                                (skuDetails));
+                                    }
+                                }
+                                setInstalledAndNotifyObserver();
+                            }
+                        }
+                    });
+        } else
+            setInstalledAndNotifyObserver();
+
+    }
+
+    private Information convertSkuDetailsToInformation(SkuDetails skuDetails) {
+        String priceString = skuDetails.getPrice();
+        return Information.newBuilder()
+                .localName(skuDetails.getTitle())
+                .localDescription(skuDetails.getDescription())
+                .localPricing(priceString)
+                .priceCurrencyCode(skuDetails.getPriceCurrencyCode())
+                .priceInCents((int) (skuDetails.getPriceAmountMicros() / 10000))
+                .build();
+    }
+
+    private void setInstalledAndNotifyObserver() {
+        if (!installationComplete) {
+            installationComplete = true;
+            observer.handleInstall();
+        }
     }
 
     @Override
     public boolean installed() {
-        return false;
+        return installationComplete;
     }
 
     @Override
@@ -44,6 +167,11 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager {
 
     @Override
     public void purchaseRestore() {
+
+    }
+
+    @Override
+    public void onPurchasesUpdated(int i, @Nullable List<Purchase> list) {
 
     }
 }
