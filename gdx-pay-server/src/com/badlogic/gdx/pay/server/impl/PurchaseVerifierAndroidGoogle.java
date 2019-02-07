498e6604582de2fccea6c499e5e1a0a2749faf17
Benjamin Schulte <bs@golfgl.de>
2019-02-07 21:31:08 +0100

    Server: Add code for verifying Google Play purchases

--- gdx-pay-server/src/com/badlogic/gdx/pay/server/impl/PurchaseVerifierAndroidGoogle.java
+++ gdx-pay-server/src/com/badlogic/gdx/pay/server/impl/PurchaseVerifierAndroidGoogle.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,18 +20,29 @@ import com.badlogic.gdx.pay.PurchaseManagerConfig;
 import com.badlogic.gdx.pay.Transaction;
 import com.badlogic.gdx.pay.server.PurchaseVerifier;
 
-/** Purchase verifier for Google Play. Return true if the purchase appears valid.
- * @author noblemaster */
+import java.security.PublicKey;
+
+/**
+ * Purchase verifier for Google Play. Return true if the purchase appears valid.
+ *
+ * @author noblemaster
+ */
 public class PurchaseVerifierAndroidGoogle implements PurchaseVerifier {
 
-	@Override
-	public String storeName () {
-		return PurchaseManagerConfig.STORE_NAME_ANDROID_GOOGLE;
-	}
+    private PublicKey publicKey;
 
-	@Override
-	public boolean isValid (Transaction transaction) {
-		// TODO: implement...
-		return true;
-	}
+    public void setPublicKey(String publicKeyString) {
+        publicKey = Security.generatePublicKey(publicKeyString);
+    }
+
+    @Override
+    public String storeName() {
+        return PurchaseManagerConfig.STORE_NAME_ANDROID_GOOGLE;
+    }
+
+    @Override
+    public boolean isValid(Transaction transaction) {
+        return Security.verify(publicKey, transaction.getTransactionData(),
+                transaction.getTransactionDataSignature());
+    }
 }
---
+++ gdx-pay-server/src/com/badlogic/gdx/pay/server/impl/Security.java
@@ -0,0 +1,69 @@
+package com.badlogic.gdx.pay.server.impl;
+
+import com.badlogic.gdx.pay.server.util.Base64Util;
+
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+public class Security {
+    private static final String KEY_FACTORY_ALGORITHM = "RSA";
+    private static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
+
+
+    /**
+     * Generates a PublicKey instance from a string containing the
+     * Base64-encoded public key.
+     *
+     * @param encodedPublicKey Base64-encoded public key
+     * @throws IllegalArgumentException if encodedPublicKey is invalid
+     */
+    public static PublicKey generatePublicKey(String encodedPublicKey) {
+        try {
+            byte[] decodedKey = Base64Util.fromBase64(encodedPublicKey);
+            KeyFactory keyFactory = KeyFactory.getInstance(KEY_FACTORY_ALGORITHM);
+            return keyFactory.generatePublic(new X509EncodedKeySpec(decodedKey));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            System.out.println("Invalid key specification.");
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Verifies that the signature from the server matches the computed
+     * signature on the data.  Returns true if the data is correctly signed.
+     *
+     * @param publicKey  public key associated with the developer account
+     * @param signedData signed data from server
+     * @param signature  server signature
+     * @return true if the data and signature match
+     */
+    public static boolean verify(PublicKey publicKey, String signedData, String signature) {
+        byte[] signatureBytes;
+        try {
+            signatureBytes = Base64Util.fromBase64(signature);
+            Signature sig = Signature.getInstance(SIGNATURE_ALGORITHM);
+            sig.initVerify(publicKey);
+            sig.update(signedData.getBytes());
+            if (!sig.verify(signatureBytes)) {
+                System.out.println("Signature verification failed.");
+                return false;
+            }
+            return true;
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("NoSuchAlgorithmException.");
+        } catch (InvalidKeyException e) {
+            System.out.println("Invalid key specification.");
+        } catch (SignatureException e) {
+            System.out.println("Signature exception.");
+        }
+        return false;
+    }
+}
