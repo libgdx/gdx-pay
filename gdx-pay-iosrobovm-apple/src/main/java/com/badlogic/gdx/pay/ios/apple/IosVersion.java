331578433789c16c4c44cce2ceb3c78110549080
Kees van Dieren <keesvandieren@squins.com>
2020-10-02 20:48:49 +0200

    #230 use logic available in iOS 11.2 and up only if available

---
+++ gdx-pay-iosrobovm-apple/src/main/java/com/badlogic/gdx/pay/ios/apple/IosVersion.java
@@ -0,0 +1,16 @@
+package com.badlogic.gdx.pay.ios.apple;
+
+import org.robovm.apple.foundation.Foundation;
+
+enum IosVersion {
+    ;
+
+    static boolean isIos_7_0_orAbove() {
+        return Foundation.getMajorSystemVersion() >= 7;
+    }
+
+    static boolean is_11_2_OrAbove() {
+        return ((Foundation.getMajorSystemVersion()  == 11 && Foundation.getMinorSystemVersion() >= 2)
+                || Foundation.getMajorSystemVersion() > 11);
+    }
+}
