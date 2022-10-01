d54d736c0b4fb2b47e05b994bd074ef8d832457a
Kees van Dieren <keesvandieren@squins.com>
2022-10-01 07:37:12 +0200

    Gdx pay android google billing 5 (#249)
    * Upgrade to Google Billing 5 (subscriptions and in-app purchases)
    
    Co-authored-by: Lucas Signorini

--- gdx-pay-android-googlebilling/src/com/badlogic/gdx/pay/android/googlebilling/PurchaseManagerGoogleBilling.java
+++ gdx-pay-android-googlebilling/src/com/badlogic/gdx/pay/android/googlebilling/PurchaseManagerGoogleBilling.java
@@ -2,6 +2,7 @@ package com.badlogic.gdx.pay.android.googlebilling;
 
 import android.app.Activity;
 import com.android.billingclient.api.*;
+import com.android.billingclient.api.BillingClient.ProductType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.pay.*;
 
@@ -20,9 +21,10 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesUpdatedListener {
     private static final String TAG = "GdxPay/GoogleBilling";
+
     private final Map<String, Information> informationMap = new ConcurrentHashMap<>();
     private final Activity activity;
-    private final Map<String, SkuDetails> skuDetailsMap = new HashMap<>();
+    private final Map<String, ProductDetails> productDetailsMap = new HashMap<>();
     private boolean serviceConnected;
     private boolean installationComplete;
     private BillingClient mBillingClient;
@@ -48,6 +50,8 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesU
 
     @Override
     public void install(PurchaseObserver observer, PurchaseManagerConfig config, boolean autoFetchInformation) {
+
+        Gdx.app.debug(TAG, "Called install()");
         this.observer = observer;
         this.config = config;
 
@@ -92,91 +96,165 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesU
         });
     }
 
-    private String getGlobalSkuTypeFromConfig() {
-        String skuType = null;
-        for (int z = 0; z < config.getOfferCount(); z++) {
-            String offerSkuType = mapOfferType(config.getOffer(z).getType());
-            if (skuType != null && !skuType.equals(offerSkuType)) {
-                throw new IllegalStateException("Cannot support OfferType Subscription and other types in the same app");
-            }
-            skuType = offerSkuType;
-        }
-
-        return skuType;
-    }
-
     private void fetchOfferDetails() {
-        skuDetailsMap.clear();
+        Gdx.app.debug(TAG,"Called fetchOfferDetails()");
+        productDetailsMap.clear();
         int offerSize = config.getOfferCount();
-        List<String> skuList = new ArrayList<>(offerSize);
+
+        List<QueryProductDetailsParams.Product> productList = new ArrayList<>();
+
         for (int z = 0; z < offerSize; z++) {
-            skuList.add(config.getOffer(z).getIdentifierForStore(storeName()));
+            Offer offer = config.getOffer(z);
+            productList.add(QueryProductDetailsParams.Product.newBuilder()
+                    .setProductId(offer.getIdentifierForStore(storeName()))
+                    .setProductType(mapOfferType(offer.getType()))
+                    .build());
         }
 
-        if (skuList.isEmpty()) {
-            Gdx.app.log(TAG, "No skus configured");
+        if (productList.isEmpty()) {
+            Gdx.app.debug(TAG, "No products configured");
             setInstalledAndNotifyObserver();
             return;
         }
 
-        mBillingClient.querySkuDetailsAsync(
-                SkuDetailsParams.newBuilder()
-                        .setSkusList(skuList)
-                        .setType(getGlobalSkuTypeFromConfig())
-                        .build(),
-                new SkuDetailsResponseListener() {
-                    @Override
-                    public void onSkuDetailsResponse(@Nonnull  BillingResult result, List<SkuDetails> skuDetailsList) {
-                        int responseCode = result.getResponseCode();
+        QueryProductDetailsParams params = QueryProductDetailsParams.newBuilder()
+                .setProductList(productList)
+                .build();
+
+        Gdx.app.debug(TAG, "QueryProductDetailsParams: " + params);
+        mBillingClient.queryProductDetailsAsync(
+                params,
+                new ProductDetailsResponseListener() {
+                    public void onProductDetailsResponse(@Nonnull BillingResult billingResult, @Nonnull List<ProductDetails> productDetailsList) {
+                        int responseCode = billingResult.getResponseCode();
                         // it might happen that this was already disposed until the response comes back
                         if (observer == null || Gdx.app == null)
                             return;
 
                         if (responseCode != BillingClient.BillingResponseCode.OK) {
-                            Gdx.app.error(TAG, "onSkuDetailsResponse failed, error code is " + responseCode);
-                            if (!installationComplete)
-                                observer.handleInstallError(new FetchItemInformationException(
-                                        String.valueOf(responseCode)));
+                            Gdx.app.error(TAG, "onProductDetailsResponse failed, error code is " + responseCode);
+                            if (!installationComplete) {
+                                observer.handleInstallError(new FetchItemInformationException(String.valueOf(responseCode)));
+                            }
 
                         } else {
-                            if (skuDetailsList != null) {
-
-                                for (SkuDetails skuDetails : skuDetailsList) {
-                                    informationMap.put(skuDetails.getSku(), convertSkuDetailsToInformation
-                                            (skuDetails));
-                                    skuDetailsMap.put(skuDetails.getSku(), skuDetails);
-                                }
-                            } else {
-                                Gdx.app.log(TAG, "skuDetailsList is null");
+                            Gdx.app.debug(TAG,"Retrieved product count: " +  productDetailsList.size());
+                            for (ProductDetails productDetails : productDetailsList) {
+                                informationMap.put(productDetails.getProductId(), convertProductDetailsToInformation
+                                        (productDetails));
+                                productDetailsMap.put(productDetails.getProductId(), productDetails);
                             }
+
                             setInstalledAndNotifyObserver();
                         }
                     }
-                });
+                }
+        );
     }
 
     private String mapOfferType(OfferType type) {
         switch (type) {
             case CONSUMABLE:
             case ENTITLEMENT:
-                return BillingClient.SkuType.INAPP;
+                return ProductType.INAPP;
             case SUBSCRIPTION:
-                return BillingClient.SkuType.SUBS;
+                return ProductType.SUBS;
         }
         throw new IllegalStateException("Unsupported OfferType: " + type);
     }
 
-    private Information convertSkuDetailsToInformation(SkuDetails skuDetails) {
-        String priceString = skuDetails.getPrice();
-        return Information.newBuilder()
-                .localName(skuDetails.getTitle())
-                .freeTrialPeriod(convertToFreeTrialPeriod(skuDetails.getFreeTrialPeriod()))
-                .localDescription(skuDetails.getDescription())
+    private Information convertProductDetailsToInformation(ProductDetails productDetails) {
+        Gdx.app.debug(TAG, "Converting productDetails: \n" + productDetails);
+
+        Information.Builder builder = Information.newBuilder()
+                .localName(productDetails.getTitle())
+                .localDescription(productDetails.getDescription());
+
+        if (ProductType.SUBS.equals(productDetails.getProductType())) {
+            convertSubscriptionProductToInformation(builder, productDetails.getSubscriptionOfferDetails());
+        } else {
+            convertOneTimeProductToInformation(builder, productDetails.getOneTimePurchaseOfferDetails());
+        }
+        return builder.build();
+    }
+
+    private void convertSubscriptionProductToInformation(Information.Builder builder, List<ProductDetails.SubscriptionOfferDetails> subscriptionOfferDetails) {
+        if (subscriptionOfferDetails.isEmpty()) {
+            Gdx.app.error(TAG, "Empty SubscriptionOfferDetails");
+            return;
+        }
+        ProductDetails.SubscriptionOfferDetails details = getActiveSubscriptionOfferDetails(subscriptionOfferDetails);
+        if (details.getPricingPhases().getPricingPhaseList().isEmpty()) {
+            Gdx.app.error(TAG, "getPricingPhases()  or empty ");
+            return;
+        }
+
+
+        ProductDetails.PricingPhase paidForPricingPhase = getPaidRecurringPricingPhase(details);
+        if (paidForPricingPhase == null) {
+            Gdx.app.error(TAG, "no paidRecurringPricingPhase found ");
+            return;
+
+        }
+
+        builder.localPricing(paidForPricingPhase.getFormattedPrice())
+                .priceCurrencyCode(paidForPricingPhase.getPriceCurrencyCode())
+                .priceInCents((int) paidForPricingPhase.getPriceAmountMicros() / 10_000)
+                .priceAsDouble(paidForPricingPhase.getPriceAmountMicros() / 1_000_000.0)
+            ;
+
+        ProductDetails.PricingPhase freeTrialSubscriptionPhase = getFreeTrialSubscriptionPhase(details.getPricingPhases());
+
+        if (freeTrialSubscriptionPhase != null) {
+            builder.freeTrialPeriod(convertToFreeTrialPeriod(freeTrialSubscriptionPhase.getBillingPeriod()));
+        }
+    }
+
+    private ProductDetails.SubscriptionOfferDetails getActiveSubscriptionOfferDetails(List<ProductDetails.SubscriptionOfferDetails> subscriptionOfferDetails) {
+        // TODO Google Play supports multiple SubscriptionOfferDetails which we don't support. Enhancement can be added here.
+        return subscriptionOfferDetails.get(0);
+    }
+
+    @Nullable
+    private ProductDetails.PricingPhase getPaidRecurringPricingPhase(ProductDetails.SubscriptionOfferDetails details) {
+        for(ProductDetails.PricingPhase phase : details.getPricingPhases().getPricingPhaseList()) {
+          if (isPaidForSubscriptionPhase(phase)) {
+              return phase;
+          }
+        }
+        return null;
+    }
+
+    private static boolean isPaidForSubscriptionPhase(ProductDetails.PricingPhase phase) {
+        return phase.getPriceAmountMicros() > 0;
+    }
+
+    @Nullable
+    private static ProductDetails.PricingPhase getFreeTrialSubscriptionPhase(ProductDetails.PricingPhases phases) {
+        for(ProductDetails.PricingPhase phase : phases.getPricingPhaseList()) {
+            if (isFreeTrialSubscriptionPhase(phase)) {
+                return phase;
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * TODO test that we can find free trial phase actually like this. Normally is non-recurring and free.
+     */
+    private static boolean isFreeTrialSubscriptionPhase(ProductDetails.PricingPhase phase) {
+        return phase.getRecurrenceMode() == ProductDetails.RecurrenceMode.NON_RECURRING && phase.getPriceAmountMicros() == 0L;
+
+    }
+
+    private static void convertOneTimeProductToInformation(Information.Builder builder, ProductDetails.OneTimePurchaseOfferDetails oneTimePurchaseDetails) {
+        String priceString = oneTimePurchaseDetails.getFormattedPrice();
+        builder
                 .localPricing(priceString)
-                .priceCurrencyCode(skuDetails.getPriceCurrencyCode())
-                .priceInCents((int) (skuDetails.getPriceAmountMicros() / 10_000))
-                .priceAsDouble(skuDetails.getPriceAmountMicros() / 1_000_000.0)
-                .build();
+                .priceCurrencyCode(oneTimePurchaseDetails.getPriceCurrencyCode())
+                .priceInCents((int) (oneTimePurchaseDetails.getPriceAmountMicros() / 10_000))
+                .priceAsDouble(oneTimePurchaseDetails.getPriceAmountMicros() / 1_000_000.0);
     }
 
     /**
@@ -214,7 +292,7 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesU
             // remove observer and config as well
             observer = null;
             config = null;
-            Gdx.app.log(TAG, "disposed observer and config");
+            Gdx.app.debug(TAG, "disposed observer and config");
         }
         if (mBillingClient != null && mBillingClient.isReady()) {
             mBillingClient.endConnection();
@@ -225,37 +303,83 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesU
 
     @Override
     public void purchase(String identifier) {
-        SkuDetails skuDetails = skuDetailsMap.get(identifier);
+        ProductDetails productDetails = productDetailsMap.get(identifier);
 
-        if (skuDetails == null) {
+        if (productDetails == null) {
             observer.handlePurchaseError(new InvalidItemException(identifier));
         } else {
-            mBillingClient.launchBillingFlow(activity, getBillingFlowParams(skuDetails).build());
+            BillingResult billingResult = mBillingClient.launchBillingFlow(activity, getBillingFlowParams(productDetails).build());
+            //billingResult.getResponseCode()
         }
     }
 
     /**
-     * @param skuDetails SKU details to set in the billing flow params.
      * @return The params builder to be used while launching the billing flow.
      */
-    protected BillingFlowParams.Builder getBillingFlowParams(SkuDetails skuDetails) {
-        return BillingFlowParams.newBuilder().setSkuDetails(skuDetails);
+    protected BillingFlowParams.Builder getBillingFlowParams(ProductDetails productDetails) {
+        final List<BillingFlowParams.ProductDetailsParams> productDetailsParamsList;
+
+        if (productDetails.getProductType().equals(ProductType.INAPP)) {
+            productDetailsParamsList =
+                    Collections.singletonList(
+                            BillingFlowParams.ProductDetailsParams.newBuilder()
+                                    .setProductDetails(productDetails)
+                                    .build()
+                    );
+        }
+        else {
+            List<ProductDetails.SubscriptionOfferDetails> subscriptionOfferDetails = productDetails
+                    .getSubscriptionOfferDetails();
+            final String offerToken;
+
+            if (subscriptionOfferDetails == null || subscriptionOfferDetails.isEmpty()) {
+                Gdx.app.error(TAG, "subscriptionOfferDetails are empty for product: " + productDetails);
+                offerToken = null;
+            } else {
+                 offerToken = getActiveSubscriptionOfferDetails(subscriptionOfferDetails) // HOW TO SPECIFY AN ALTERNATE OFFER USING gdx-pay?
+                        .getOfferToken();
+            }
+
+            productDetailsParamsList =
+                    Collections.singletonList(
+                            BillingFlowParams.ProductDetailsParams.newBuilder()
+                                    .setProductDetails(productDetails)
+                                    .setOfferToken(offerToken)
+                                    .build()
+                    );
+        }
+
+        return BillingFlowParams.newBuilder().setProductDetailsParamsList(productDetailsParamsList);
     }
+
+
+
 
     @Override
     public void purchaseRestore() {
-        Purchase.PurchasesResult purchasesResult = mBillingClient.queryPurchases(getGlobalSkuTypeFromConfig());
-        int responseCode = purchasesResult.getResponseCode();
-        List<Purchase> purchases = purchasesResult.getPurchasesList();
-
-        if (responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
-            handlePurchase(purchases, true);
+        final String productType;
+        if (this.config.hasAnyOfferWithType(OfferType.SUBSCRIPTION)) {
+            productType = ProductType.SUBS;
         } else {
-            Gdx.app.error(TAG, "queryPurchases failed with responseCode " + responseCode);
-            observer.handleRestoreError(new GdxPayException("queryPurchases failed with " +
-                    "responseCode " + responseCode));
+            productType = ProductType.INAPP;
         }
 
+        mBillingClient.queryPurchasesAsync(
+                QueryPurchasesParams.newBuilder().setProductType(productType).build(),
+
+                new PurchasesResponseListener() {
+                    @Override
+                    public void onQueryPurchasesResponse(@Nonnull BillingResult billingResult, @Nonnull  List<Purchase> list) {
+                        int responseCode = billingResult.getResponseCode();
+                        if (responseCode == BillingClient.BillingResponseCode.OK) {
+                            handlePurchase(list, true);
+                        } else {
+                            Gdx.app.error(TAG, "queryPurchases failed with responseCode " + responseCode);
+                            observer.handleRestoreError(new GdxPayException("queryPurchases failed with " +
+                                    "responseCode " + responseCode));
+                        }
+                    }
+                });
     }
 
     @Override
@@ -288,15 +412,17 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesU
         for (Purchase purchase : purchases) {
             // ignore pending purchases, just return successful purchases to the client
             if (purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED) {
+                //TODO - Only handling one product per purchase. This can be changed at Play Store - In app Product Settings
+                String product = purchase.getProducts().get(0);
 
                 // build the transaction from the purchase object
                 Transaction transaction = new Transaction();
-                transaction.setIdentifier(purchase.getSku());
+                transaction.setIdentifier(product);
                 transaction.setOrderId(purchase.getOrderId());
                 transaction.setRequestId(purchase.getPurchaseToken());
                 transaction.setStoreName(PurchaseManagerConfig.STORE_NAME_ANDROID_GOOGLE);
                 transaction.setPurchaseTime(new Date(purchase.getPurchaseTime()));
-                transaction.setPurchaseText("Purchased: " + purchase.getSku());
+                transaction.setPurchaseText("Purchased: " + product);
                 transaction.setReversalTime(null);
                 transaction.setReversalText(null);
                 transaction.setTransactionData(purchase.getOriginalJson());
@@ -309,7 +435,7 @@ public class PurchaseManagerGoogleBilling implements PurchaseManager, PurchasesU
                 else
                     observer.handlePurchase(transaction);
 
-                Offer purchasedOffer = config.getOffer(purchase.getSku());
+                Offer purchasedOffer = config.getOffer(product);
                 if (purchasedOffer != null) {
                     // CONSUMABLES need to get consumed, ENTITLEMENTS/SUBSCRIPTIONS need to geed acknowledged
                     switch (purchasedOffer.getType()) {
